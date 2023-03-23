dd443d60eae346b46d31b44109e50602b999f5e8
DiegoJord <diegojordfullstack@outlook.com>
2023-03-23 16:42:03 -0300

    segundocommit

--- questao5.py
+++ questao5.py
@@ -1,8 +1,11 @@
 '''5. Crie um programa em Python em que as quatro operações em que todos os resultados resultam em 8.'''
 
 soma = 4 + 4
-multiplicação = soma * 2
-subtração = multiplicação - 8
-divisão = subtração / 1
+multiplicação = 4 * 2
+subtração = 16 - 8
+divisão = 16 / 2
 
+print(soma)
+print(multiplicação)
+print(subtração)
 print(divisão)
