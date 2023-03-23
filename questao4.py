cef3615c970266a95e56ed594955168ea8ce873a
DiegoJord <diegojordfullstack@outlook.com>
2023-03-23 16:31:54 -0300

    segundocommit

---
+++ questao4.py
@@ -0,0 +1,6 @@
+'''4. Crie um programa em Python que imprima um convite para uma festa com o nome do usuário que for
+digitado. O nome do usuário precisará ser apresentado todo em caixa alta.'''
+
+nome = str(input("Bem vindo a festa, vamos checar se você foi convidado, digite aqui o seu nome: "))
+sobrenome = str(input("Digite o seu sobrenome: "))
+print(f"Muito bem {nome} {sobrenome}! Você é um dos convidados, pode passar!")
---
+++ questao5.py
@@ -0,0 +1,8 @@
+'''5. Crie um programa em Python em que as quatro operações em que todos os resultados resultam em 8.'''
+
+soma = 4 + 4
+multiplicação = soma * 2
+subtração = multiplicação - 8
+divisão = subtração / 1
+
+print(divisão)
